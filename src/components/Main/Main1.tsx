45d1a5a6486a093f1f17310f246b9769608874af
kweonsikyung <ekrk6632@naver.com>
2023-05-11 15:53:37 +0900

    add: temporary layout

---
+++ src/components/Main/Main1.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const Main1 = () => {
+  return (
+    <>
+      <Container>
+            <div className='col-lg-12 mt-100 mb-100'>
+                <div>생태문화길을 걸으며,</div>
+                <div  className='mb-50'>서울을 느끼다. </div>
+                <img src='../assets/img/logo.svg' width="50%"></img>
+            </div>
+      </Container>
+    </>
+  );
+};
+
+export default Main1;
+
+const Container = styled.div`
+    font-size:45px;
+    margin: auto;
+    font-weight: 200;
+    font-family: var(--font-secondary);
+    text-align: center;
+`;
